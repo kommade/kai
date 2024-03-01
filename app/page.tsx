83f249ff7d5088d907b2311617e8bc43f8e7bd89
kommade <99523586+kommade@users.noreply.github.com>
2024-03-01 13:23:38 +0800

    header design

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,35 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <div className="w-screen h-fit min-h-[100vh] relative flex flex-col">
+                <header className="top-0 left-0 w-full h-fit">
+                    <div className="w-full h-[120px] py-[30px] px-[20px] flex justify-between">
+                        <h1 className="w-fit ml-[30px]">KAI<br />studios</h1>
+                        <nav className="flex justify-end items-center">
+                            <ul className="flex gap-4 mr-[30px] font-['Breathing'] text-[12px]">
+                                <li>Our Products</li>
+                                <li>About us</li>
+                                <li>Shopping cart icon</li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <svg className="fill-[#f2f1eb] absolute top-0 left-0 z-[1001]" width={470} height={480} viewBox="30 30 470 480">
+                        <path d="M263.99507085643864,54.92380365578147C228.71554734031628,107.04128157732593 232.9250359416718,151.44136372971863 193.13534606695418,175.09468063257341C112.95461080303964,222.90244403368249 30.76935715752721,177.29965085233107 5.512425549394126,259.2844526596837C-20.345861573218322,343.2737728486342 45.903470938591084,424.4567673033477 184.61614294516326,475.2713082768535C346.3807763401109,534.7052782912302 511.5530909837749,372.9406448962826 472.7656602998562,252.56931608133087C440.6933661942904,152.84452659683714 496.2185253645513,109.1460258780037 462.8432943109468,54.92380365578147C408.1199424933251,-33.77613472992401 301.7802423495584,-1.002259190798932 263.99507085643864,54.92380365578147Z"></path>
+                    </svg>
+                    <img className="absolute top-[180px] left-[120px] z-[1002]" src="images/logo.png"/>
+                    <svg className="absolute left-[400px] top-[150px] scale-150" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="390" height="260" viewBox="0 0 390 200" fill="white">
+                        <defs>
+                            <clipPath id="headerClip">
+                                <path d="M138.55220064208316,17.020119055131527C134.3397713775161,18.755767928309034 130.18979185199797,20.62643276076146 126.11339240981738,22.60422023513537C112.46470347023734,29.22654794927265 99.01082998879387,33.86998008800265 83.60504569907623,32.23783405332548C63.68026782863469,30.126943367580715 40.646246756246974,27.994809782950487 26.474512683744674,42.15939254632142C16.08005017651987,52.54872099975784 14.087864839146045,68.74656817579138 15.542209689876294,83.37116258146449C16.996452751569223,97.99576463893467 21.283272384510795,112.35697955815674 21.13104260023318,127.05295717595638C20.937129651424527,145.77962229956742 13.585262790952505,163.58554970236364 9.467488471117512,181.85492621661348C5.349612865652904,200.12430273086338 5.072134540376853,221.09877355470866 16.996151311037337,235.53932945432703C32.82137801908885,254.70455591738164 62.73607886002777,253.90364795638064 86.19739641703254,245.70141151593023C109.65881002422682,237.4990982554645 131.66628221162313,223.5755460929381 156.44934078842755,221.70366774587956C194.4740437106785,218.83153380014335 229.68529854753686,244.9216427518281 267.816926373164,244.5935244308261C316.0331157029295,244.17871386675725 356.09781198791256,197.25275263737984 356.9902667538127,149.04069578143904C357.8825986882336,100.82863902617976 325.66198631307094,55.931218130908995 283.344122733784,32.818163257504665C261.2057673629821,20.72671519951136 238.34935557552532,8.928348789876948 213.05623716678707,5.392613649502862C187.59054475831525,1.832813204621938 162.09717247735549,7.318749077306927 138.55220064208316,17.020119055131527Z"></path>
+                            </clipPath>
+                        </defs>
+                        <image className="object-fill object-[50%_90%]" xlinkHref="images/header-image.webp" x="0" y="-130" height="465" width="390" clipPath="url(#headerClip)"/>
+                    </svg>
+                </header>
+                <div className="bg-white w-full h-[100px] mt-[354px]"></div>
+            </div>
+        </main>
+    );
 }
