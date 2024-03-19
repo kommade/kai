d1427a65a2d093deec89e1557e2e4d4f106c0fe7
kommade <99523586+kommade@users.noreply.github.com>
2024-03-19 23:59:39 +0800

    .env instead of .env.local?

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,25 +1,26 @@
 import { test, expect } from '@playwright/test';
 import { config } from 'dotenv';
+config();
 config({ path: '.env.local' });
 
 // TODO: run on vercel's preview deployment
 
 test('product page', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('/');
     await page.getByRole('button', { name: 'Our Products' }).click();
-    await page.waitForURL('http://localhost:3000/products');
+    await page.waitForURL('/products');
     await page.getByRole('button', { name: 'Filters' }).click();
     await expect(page.getByText('By Type')).toBeVisible();
     await expect(page.getByText('By Collection')).toBeVisible();
     await page.getByRole('menuitem', { name: 'Kai Shi (开始)' }).click();
-    await page.waitForURL('http://localhost:3000/products?collection=Kai+Shi+%28%E5%BC%80%E5%A7%8B%29');
+    await page.waitForURL('/products?collection=Kai+Shi+%28%E5%BC%80%E5%A7%8B%29');
     await page.getByText('BUY NOWKai Shi (开始) Sunflower16.00').click();
     await page.getByRole('button', { name: '+' }).click();
     await expect(page.locator('section')).toContainText('2');   
 });
 
 test('purchase process', async ({ page, browserName }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('/');
     await page.getByRole('button', { name: 'Our Products' }).click();
     await page.getByText('BUY NOWKai Shi (开始) Sunflower16.00').click();
     await page.waitForTimeout(1000);
@@ -31,7 +32,7 @@ test('purchase process', async ({ page, browserName }) => {
     await expect(page.locator('tbody')).toContainText('2');
     await expect(page.getByRole('row', { name: 'Sunflower Kai Shi (开始) -' }).getByRole('button').nth(2)).toBeVisible();
     await page.getByRole('button', { name: 'Checkout' }).click();
-    await page.waitForURL('http://localhost:3000/checkout');
+    await page.waitForURL('/checkout');
     test.skip(browserName === 'webkit', 'Stripe does not work with webkit')
     await page.waitForTimeout(2000)
     await page.frameLocator('iframe[name="embedded-checkout"]').getByRole('button', { name: 'Enter address manually' }).click();
@@ -44,25 +45,25 @@ test('purchase process', async ({ page, browserName }) => {
     await page.frameLocator('iframe[name="embedded-checkout"]').getByPlaceholder('MM / YY').fill('11 / 24');
     await page.frameLocator('iframe[name="embedded-checkout"]').getByPlaceholder('CVC').fill('000');
     await page.frameLocator('iframe[name="embedded-checkout"]').getByTestId('hosted-payment-submit-button').click();
-    await page.waitForURL('http://localhost:3000/return?session_id=cs_test_**', { timeout: 0 });
+    await page.waitForURL('**/return?session_id=cs_test_**', { timeout: 0 });
     await page.getByRole('button', { name: 'Continue Shopping' }).click();
-    await page.waitForURL('http://localhost:3000/');
+    await page.waitForURL('/');
 });
 
 test('login admin', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('/');
     await page.getByLabel('Main').getByRole('link').nth(3).click();
     await page.getByPlaceholder('Email').fill(process.env.WEBSITE_ADMIN_EMAIL!);
     await page.getByPlaceholder('Password').fill(process.env.WEBSITE_ADMIN_PASSWORD!);
     await page.getByRole('button', { name: 'Login' }).click();
-    await page.waitForURL('http://localhost:3000/dashboard');
+    await page.waitForURL('/dashboard');
     await expect(page.locator('section')).toContainText(/^DASHBOARDThere are [0-9]+ pending order\(s\).View Orders$/g, );
     await page.getByRole('button', { name: 'View Orders' }).click();
-    await page.waitForURL('http://localhost:3000/dashboard/orders');
+    await page.waitForURL('/dashboard/orders');
     await page.locator('body > main > div > section > div > div > div.rounded-md.border > div > table > tbody > tr:nth-child(1)').getByRole('button').click();
     await expect(page.getByLabel('Open menu')).toBeVisible();
     await page.getByRole('menuitem', { name: 'View order' }).click();
-    await page.waitForURL('http://localhost:3000/dashboard/orders/1');
+    await page.waitForURL('/dashboard/orders/1');
     await expect(page.getByRole('heading', { name: 'Payment' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Customer' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Shipping' })).toBeVisible();
