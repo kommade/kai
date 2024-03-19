49e709798a6c366053ed9017a04739a9f90040c5
kommade <99523586+kommade@users.noreply.github.com>
2024-03-19 23:36:20 +0800

    one more time?

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,5 +75,6 @@ export default defineConfig({
         command: 'pnpm run test',
         url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI,
+        timeout: 120 * 1000,
     },
 });
