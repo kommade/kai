85dcba8bd968aae10aa70ae9bd78545ae1ffd5fd
kommade <99523586+kommade@users.noreply.github.com>
2024-03-19 23:53:44 +0800

    PLEASEWORKIWANTTOSLEEP

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,5 +76,7 @@ export default defineConfig({
         url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI,
         timeout: 120 * 1000,
+        stdout: "pipe",
+        stderr: "pipe",
     },
 });
