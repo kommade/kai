4bbe583e56b4d6a89de5f255f14ef78a1502f801
kommade <99523586+kommade@users.noreply.github.com>
2024-04-10 14:44:23 +0800

    help

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react'
 import OrdersPage from "./page-client"
 import LoadingComponent from "@/components/LoadingComponent";
 import { ifLoggedInGetUser } from "@/functions/auth";
-import { getAllOrders, getNumberOfOrders } from "@/functions/database";
+import { getAllOrders } from "@/functions/database";
 
 const OrdersPageWrapper = async () => {
     const auth = await ifLoggedInGetUser();
